9a3088cba845957e09d6dae9b4847b7874860815
alexg <alexg@DESKTOP-8VAG3TG>
2019-09-11 14:46:08 +0300

    Added GUI and function for changin' the size of the matrix.

---
+++ OOPLabs/lab1/MatrixForm.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace lab1
+{
+    partial class MatrixForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewMatrix = new System.Windows.Forms.DataGridView();
+            this.numericUpDownM = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDownN = new System.Windows.Forms.NumericUpDown();
+            this.buttonSetMN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewMatrix
+            // 
+            this.dataGridViewMatrix.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewMatrix.Location = new System.Drawing.Point(12, 42);
+            this.dataGridViewMatrix.Name = "dataGridViewMatrix";
+            this.dataGridViewMatrix.Size = new System.Drawing.Size(566, 288);
+            this.dataGridViewMatrix.TabIndex = 0;
+            // 
+            // numericUpDownM
+            // 
+            this.numericUpDownM.Location = new System.Drawing.Point(13, 13);
+            this.numericUpDownM.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownM.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownM.Name = "numericUpDownM";
+            this.numericUpDownM.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownM.TabIndex = 1;
+            this.numericUpDownM.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // numericUpDownN
+            // 
+            this.numericUpDownN.Location = new System.Drawing.Point(139, 13);
+            this.numericUpDownN.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownN.Name = "numericUpDownN";
+            this.numericUpDownN.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDownN.TabIndex = 2;
+            this.numericUpDownN.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonSetMN
+            // 
+            this.buttonSetMN.Location = new System.Drawing.Point(266, 13);
+            this.buttonSetMN.Name = "buttonSetMN";
+            this.buttonSetMN.Size = new System.Drawing.Size(178, 23);
+            this.buttonSetMN.TabIndex = 3;
+            this.buttonSetMN.Text = "Изменить размерность";
+            this.buttonSetMN.UseVisualStyleBackColor = true;
+            this.buttonSetMN.Click += new System.EventHandler(this.buttonSetMN_Click);
+            // 
+            // MatrixForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 342);
+            this.Controls.Add(this.buttonSetMN);
+            this.Controls.Add(this.numericUpDownN);
+            this.Controls.Add(this.numericUpDownM);
+            this.Controls.Add(this.dataGridViewMatrix);
+            this.Name = "MatrixForm";
+            this.Text = "Matrix";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewMatrix)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownM)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownN)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridViewMatrix;
+        private System.Windows.Forms.NumericUpDown numericUpDownM;
+        private System.Windows.Forms.NumericUpDown numericUpDownN;
+        private System.Windows.Forms.Button buttonSetMN;
+    }
+}
+
---
+++ OOPLabs/lab1/MatrixForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class MatrixForm : Form
+    {
+        int M, N;
+        public MatrixForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSetMN_Click(object sender, EventArgs e)
+        {
+            M = (int)numericUpDownM.Value;
+            N = (int)numericUpDownN.Value;
+
+            dataGridViewMatrix.RowCount = M;
+            dataGridViewMatrix.ColumnCount = N;
+        }
+    }
+}
